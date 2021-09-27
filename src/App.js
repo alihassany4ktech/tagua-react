b42601a28414af3d1911476845525683788b10f4
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-09-27 19:53:06 +0500

    new
    new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Login from "./Componenets/Auth/Login";
 import MainDahsboard from "./Componenets/Dashboard/MainDashboard";
-import { Switch, Route, Redirect, Link } from "react-router";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 import Signup from "./Componenets/Auth/Signup";
 import Home from "./Componenets/Frontend/Home/Home";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import '../node_modules/font-awesome/css/font-awesome.min.css';  
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import "react-image-gallery/styles/css/image-gallery.css";
 import VerifyEmail from "./Componenets/Auth/VerifyEmail";
 import VerifyOtp from "./Componenets/Auth/VerifyOtp";
-import ResetPassword from "./Componenets/Auth/ResetPassword"; 
+import ResetPassword from "./Componenets/Auth/ResetPassword";
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Switch >
         <Route exact path="/" component={Login} />
         <Route exact path="/verify-email" component={VerifyEmail} />
@@ -20,10 +20,10 @@ const App = () => {
         <Route exact path="/reset-password" component={ResetPassword} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/dashboard" component={MainDahsboard} />
-        <Route exact path="/home" component={Home} /> 
+        <Route exact path="/home" component={Home} />
         <Redirect to="/" />
       </Switch>
-    </>
+    </BrowserRouter>
 
 
   );
