29b227fa5a6bcfd4d4e61e1de194dda1a6793361
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-10-04 15:19:43 +0500

    new
    new

--- src/Componenets/Account/Account.js
+++ src/Componenets/Account/Account.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./account.css";
 const Account = () => {
 
       return (
@@ -8,7 +8,7 @@ const Account = () => {
                   <div className="container-fluid p-0">
                         <div className="row mt-5 mb-5">
                               <div className="col-12 col-md-3" style={{ borderRight: "1px solid #ebebeb" }}>
-                                    <h3>My account </h3>
+                                    <h5 className="mx-5">My account </h5>
                                     <hr />
 
                                     <nav id="app-nav-main" className="app-nav app-nav-main flex-grow-1">
@@ -41,13 +41,13 @@ const Account = () => {
                                                 <li className="nav-item">
                                                       <a href="#" className="nav-link">
 
-                                                            <span className="nav-link-text">Payment method</span>
+                                                            <span className="nav-link-text">Payment methods</span>
                                                       </a>
                                                 </li>
                                                 <li className="nav-item">
                                                       <a href="#" className="nav-link">
 
-                                                            <span className="nav-link-text">Account detail</span>
+                                                            <span className="nav-link-text">Account details</span>
                                                       </a>
                                                 </li>
                                                 <li className="nav-item">
@@ -68,24 +68,41 @@ const Account = () => {
 
 
                               </div>
-                              <div className="col-12 col-md-9">
+                              <div className="col-12 col-md-9 p-4">
 
                                     <p>Hello peter (not peter? Log out)</p>
 
                                     <p>From your account dashboard you can view your recent orders, manage your shipping and billing
                                           addresses, and <Link exact to="#"> edit your password and account details</Link>.</p>
-                                    <div className="d-flex flex-row">
-                                          <div className="col-md-4 mb-4 mt-2 text-center" style={{ border: "1px solid #dbdddc" }}>
-                                                <i class="fa fa-envelope mt-4" style={{ fontSize: '50px' }}></i>
-                                                <p className="mb-5">info@taguagunleather.com</p>
+                                    <div className="account-responsive-grid mt-4">
+                                          <div className="text-center shadow border-0 account-div" style={{ border: "1px solid #dbdddc", height: "130px" }}>
+                                                <i class="bi bi-list-check mt-4 " style={{ fontSize: '50px' }}></i>
+                                                <p className="mb-5">ORDERS</p>
                                           </div>
-                                          <div className="col-md-4 mb-4 mt-2 m-2 text-center" style={{ border: "1px solid #dbdddc" }}>
-                                                <i class="fa fa-envelope mt-4" style={{ fontSize: '50px' }}></i>
-                                                <p className="mb-5">info@taguagunleather.com</p>
+                                          <div className="text-center shadow border-0 account-div" style={{ border: "1px solid #dbdddc", height: "130px" }}>
+                                                <i class="bi bi-download mt-4 " style={{ fontSize: '50px' }}></i>
+                                                <p className="mb-5">DOWNLOADS</p>
+
                                           </div>
-                                          <div className="col-md-4 mb-4 mt-2 text-center" style={{ border: "1px solid #dbdddc" }}>
-                                                <i class="fa fa-envelope mt-4" style={{ fontSize: '50px' }}></i>
-                                                <p className="mb-5">info@taguagunleather.com</p>
+                                          <div className="text-center shadow border-0 account-div" style={{ border: "1px solid #dbdddc", height: "130px" }}>
+                                                <i class="bi bi-geo-alt mt-4" style={{ fontSize: '50px' }}></i>
+                                                <p className="mb-5">ADDRESSES</p>
+                                          </div>
+                                          <div className="text-center shadow border-0 account-div" style={{ border: "1px solid #dbdddc", height: "130px" }}>
+                                                <i class="bi bi-credit-card-2-front mt-4" style={{ fontSize: '50px' }}></i>
+                                                <p className="mb-5">PAYMENT METHODS</p>
+                                          </div>
+                                          <div className="text-center shadow border-0 account-div" style={{ border: "1px solid #dbdddc", height: "130px" }}>
+                                                <i class="bi bi-person-circle mt-4" style={{ fontSize: '50px' }}></i>
+                                                <p className="mb-5">ACCOUNT DETAILS</p>
+                                          </div>
+                                          <div className="text-center shadow border-0 account-div" style={{ border: "1px solid #dbdddc", height: "130px" }}>
+                                                <i class="bi bi-heart mt-4" style={{ fontSize: '50px' }}></i>
+                                                <p className="mb-5">WISHLIST</p>
+                                          </div>
+                                          <div className="text-center shadow border-0  account-div" style={{ border: "1px solid #dbdddc", height: "130px" }}>
+                                                <i class="bi bi-box-arrow-right mt-4" style={{ fontSize: '50px' }}></i>
+                                                <p className="mb-5">LOGOUT</p>
                                           </div>
                                     </div>
 
