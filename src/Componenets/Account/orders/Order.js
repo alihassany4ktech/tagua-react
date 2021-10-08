c0b8b28b5cdf2045d3cf69bca6852401532064f7
faizy4ktech <faiz@webfabricant.com>
2021-10-08 19:25:05 +0500

    nested routes
    nested routes

--- src/Componenets/Account/orders/Order.js
+++ src/Componenets/Account/orders/Order.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import '../account.css';
 import ReactPaginate from 'react-paginate';
 const Order = ({ match }) => { 
       return (
